4ce3eb9ff9097a6bbcc793098938d82f4e149e5e
Netanel Mizrahi <nmiz1987@gmail.com>
2025-04-22 14:27:37 +0300

    🌟 Major feature addition and UI enhancements! 🌟
    - Introduced a new `NewScreen` for adding plants, complete with validation for user input! 🌱
    - Integrated `react-native-keyboard-aware-scroll-view` to improve user experience during data entry! 📱
    - Updated `OnboardingScreen` with a new heading and tagline for better branding! 🎉
    - Enhanced layout with a new modal screen for plant addition, ensuring a seamless navigation experience! 🔄
    - Added `PlantlyImage` component for consistent image rendering across the app! 🖼️
    - Expanded state management with `plantsStore` to handle plant data persistently using Zustand and AsyncStorage! 🔒
    - Updated theme colors to include `colorLightGrey` for improved styling options! 🎨
    - Added a new icon button in the tab layout for quick access to the plant addition screen! ➕
    - Included a new plant image asset to enhance visual appeal! 🌿
    
    This commit significantly boosts functionality and user engagement! Let's keep the momentum going! 🚀

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,10 +1,11 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { theme } from '@/theme';
 import { useUserStore } from '@/store/userStore';
 import { useRouter } from 'expo-router';
 import { PlantlyButton } from '@/components/PlantlyButton';
 import { StatusBar } from 'expo-status-bar';
+import { PlantlyImage } from '@/components/PlantlyImage';
 
 export default function OnboardingScreen() {
   const router = useRouter();
@@ -23,6 +24,11 @@ export default function OnboardingScreen() {
       style={styles.container}
     >
       <StatusBar style="light" />
+      <View>
+        <Text style={styles.heading}>Plantly</Text>
+        <Text style={styles.tagline}>Keep your plants healthy and hydrated</Text>
+      </View>
+      <PlantlyImage />
       <PlantlyButton title="Let me in!" onPress={handlePress} />
     </LinearGradient>
   );
@@ -31,8 +37,21 @@ export default function OnboardingScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
     backgroundColor: theme.colorWhite,
+    paddingHorizontal: 8,
+  },
+  heading: {
+    fontSize: 42,
+    color: theme.colorWhite,
+    fontWeight: 'bold',
+    marginBottom: 12,
+    textAlign: 'center',
+  },
+  tagline: {
+    fontSize: 24,
+    color: theme.colorWhite,
+    textAlign: 'center',
   },
 });
