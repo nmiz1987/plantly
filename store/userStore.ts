23d66698c14eee6acab05082b306c03e0522ae3a
Netanel Mizrahi <nmiz1987@gmail.com>
2025-07-04 16:49:22 +0300

    ✨ Refactor user store implementation for improved clarity and maintainability! 🎉
    - Introduced a `persistConfig` object to encapsulate persistence settings, enhancing code organization and readability! 📚
    - Created a separate `userStore` function to define the state and actions, promoting modularity and reusability! 🔄
    - Updated the `useUserStore` export to utilize the new structure, ensuring a cleaner and more maintainable state management solution! 🚀
    
    These changes significantly enhance the structure of the user store, paving the way for easier future modifications and improved code quality! Let's keep the momentum going! 🌟

--- store/userStore.ts
+++ store/userStore.ts
@@ -1,28 +1,27 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { create } from 'zustand';
-import { persist, createJSONStorage } from 'zustand/middleware';
+import { create, StateCreator } from 'zustand';
+import { persist, createJSONStorage, PersistOptions } from 'zustand/middleware';
 
 type UserState = {
   hasFinishedOnboarding: boolean;
   toggleHasOnboarded: () => void;
 };
 
-export const useUserStore = create(
-  persist<UserState>(
-    set => ({
-      hasFinishedOnboarding: false,
-      toggleHasOnboarded: () => {
-        return set(state => {
-          return {
-            ...state,
-            hasFinishedOnboarding: !state.hasFinishedOnboarding,
-          };
-        });
-      },
-    }),
-    {
-      name: 'plantly-user-store',
-      storage: createJSONStorage(() => AsyncStorage),
-    },
-  ),
-);
+const persistConfig: PersistOptions<UserState> = {
+  name: 'plantly-user-store',
+  storage: createJSONStorage(() => AsyncStorage),
+};
+
+const userStore: StateCreator<UserState> = set => ({
+  hasFinishedOnboarding: false,
+  toggleHasOnboarded: () => {
+    return set(state => {
+      return {
+        ...state,
+        hasFinishedOnboarding: !state.hasFinishedOnboarding,
+      };
+    });
+  },
+});
+
+export const useUserStore = create(persist<UserState>(userStore, persistConfig));
